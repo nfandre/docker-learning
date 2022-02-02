9de459b5b56aabd0ff9313aecf94a1263a3925b3
nfandre <andrelive.05@hotmail.com>
2022-02-02 17:49:16 -0300

    salvando arquivos

---
+++ docker-images/cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "Starting httpd..."
+apachectl -DFOREGROUND 
+
+#if [ "$?" = 0 ]; then
+#  echo "This went good"
+#else
+#  echo "There was an error starting the service"
+#fi
+ 
